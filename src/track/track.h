cfa9202d794bdd36e99f4342d7b2712bbf208ec1
ronso0 <ronso0@mixxx.org>
2024-06-23 13:00:26 +0200

    Track: add swapHotcues()

--- src/track/track.h
+++ src/track/track.h
@@ -325,6 +325,7 @@ class Track : public QObject {
     }
     CuePointer findCueByType(mixxx::CueType type) const; // NOTE: Cannot be used for hotcues.
     CuePointer findCueById(DbId id) const;
+    CuePointer findHotcueByIndex(int idx) const;
     void removeCue(const CuePointer& pCue);
     void removeCuesOfType(mixxx::CueType);
     QList<CuePointer> getCuePoints() const {
@@ -332,7 +333,7 @@ class Track : public QObject {
         // lock thread-unsafe copy constructors of QList
         return m_cuePoints;
     }
-
+    void swapHotcues(int a, int b);
     void setCuePoints(const QList<CuePointer>& cuePoints);
 
 #ifdef __STEM__
