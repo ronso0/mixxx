1e94a2f986c0d18f449d9d6a8beecc4644ee1d22
ronso0 <ronso0@mixxx.org>
2025-03-03 12:57:17 +0100

    Tracks: add hotkey Shift+Del to purge and move track files to trash

--- src/widget/wtracktableview.cpp
+++ src/widget/wtracktableview.cpp
@@ -1193,6 +1193,8 @@ void WTrackTableView::keyPressEvent(QKeyEvent* event) {
     case kHideRemoveShortcutKey: {
         if (event->modifiers() == kHideRemoveShortcutModifier) {
             hideOrRemoveSelectedTracks();
+        } else if (event->modifiers().testFlag(Qt::ShiftModifier)) {
+            slotDeleteTracksFromDisk();
         }
         return;
     }
