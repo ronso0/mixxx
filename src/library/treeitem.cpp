5041770cea02da18f45fd0f657615e8c3210f9d3
ronso0 <ronso0@mixxx.org>
2025-04-24 11:18:07 +0200

    -+ Browse feature: always use absolutePath() for TreeItems so the track (directory) filter works as expected

--- src/library/treeitem.cpp
+++ src/library/treeitem.cpp
@@ -32,11 +32,13 @@ TreeItem::TreeItem(
         LibraryFeature* pFeature,
         QString label,
         QVariant data)
-    : m_pFeature(pFeature),
-      m_pParent(nullptr),
-      m_label(std::move(label)),
-      m_data(std::move(data)),
-      m_bold(false) {
+        : m_pFeature(pFeature),
+          m_pParent(nullptr),
+          m_label(std::move(label)),
+          m_data(std::move(data)),
+          m_bold(false),
+          m_needsUpdate(false),
+          m_isWatched(false) {
 }
 
 TreeItem::~TreeItem() {
