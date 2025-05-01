847b8d9c4d55e343a81f37f295439871ee89afe9
ronso0 <ronso0@mixxx.org>
2025-05-01 22:12:23 +0200

    -+-Browse feature: use Library model when path is watched (in library)
    currently uses recursive mode

---
+++ src/library/browse/browselibrarytablemodel.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "library/basesqltablemodel.h"
+
+class BrowseLibraryTableModel : public BaseSqlTableModel {
+    Q_OBJECT
+  public:
+    BrowseLibraryTableModel(
+            QObject* pParent,
+            TrackCollectionManager* pTrackCollectionManager);
+    ~BrowseLibraryTableModel() override = default;
+
+    void setTableModel(int id = -1); // param required to avoid override?
+    void setPath(QString path);
+
+    bool isColumnInternal(int column) final;
+    Capabilities getCapabilities() const final;
+};
