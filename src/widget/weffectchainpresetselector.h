d802c92c78b2f198a811eab46ec53d771221dd09
ronso0 <ronso0@mixxx.org>
2025-07-06 21:09:00 +0200

    Effects / LateNight: show QuickEffect preset name tooltip when preset is changed
    requires skin refactoring (singleton for QuickEffect chain selector) because we'd get one tolltip per selector

--- src/widget/weffectchainpresetselector.h
+++ src/widget/weffectchainpresetselector.h
@@ -35,4 +35,5 @@ class WEffectChainPresetSelector : public QComboBox, public WBaseWidget {
     EffectChainPresetManagerPointer m_pChainPresetManager;
     EffectsManager* m_pEffectsManager;
     EffectChainPointer m_pChain;
+    QPoint m_lastTopLeft;
 };
