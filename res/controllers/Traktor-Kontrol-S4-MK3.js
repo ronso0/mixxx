f8e3a7bef54db653054d90415c7de2dc33083477
ronso0 <ronso0@mixxx.org>
2025-10-29 01:06:02 +0100

    Traktor S4 Mk3: don't enable scratching when switching to TT mode

--- res/controllers/Traktor-Kontrol-S4-MK3.js
+++ res/controllers/Traktor-Kontrol-S4-MK3.js
@@ -3295,9 +3295,6 @@ class S4Mk3Deck extends Deck {
                     this.deck.wheelMode = WheelModes.motor;
                     engine.setValue(this.group, "scratch2_enable", false);
                     const group = this.group;
-                    engine.beginTimer(MotorWindUpMilliseconds, () => {
-                        engine.setValue(group, "scratch2_enable", true);
-                    }, true);
                 }
                 this.outTrigger();
             } : undefined,
