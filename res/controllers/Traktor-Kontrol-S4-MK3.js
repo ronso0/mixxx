f5e9848bf0cce6f2b45d6b805bf111b087c45c1b
ronso0 <ronso0@mixxx.org>
2025-11-01 18:05:10 +0100

    Traktor S4 Mk3: default to motor mode if motors are enabled

--- res/controllers/Traktor-Kontrol-S4-MK3.js
+++ res/controllers/Traktor-Kontrol-S4-MK3.js
@@ -2987,7 +2987,7 @@ class S4Mk3Deck extends Deck {
             }
         });
 
-        this.wheelMode = WheelModes.vinyl;
+        this.wheelMode = UseMotors ? WheelModes.motor : WheelModes.vinyl;
         this.turntableButton = new Button({
             deck: this,
             onShortPress: function() {
