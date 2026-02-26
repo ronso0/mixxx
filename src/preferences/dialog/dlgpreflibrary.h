bab5ad31d377088a217831f152f6a1b8cf5e3298
ronso0 <ronso0@mixxx.org>
2026-02-26 11:57:03 +0100

    Preferences Library: fix applying date format

--- src/preferences/dialog/dlgpreflibrary.h
+++ src/preferences/dialog/dlgpreflibrary.h
@@ -78,7 +78,7 @@ class DlgPrefLibrary : public DlgPreferencePage, public Ui::DlgPrefLibraryDlg {
     void resetLibraryFont();
     void updateSearchLineEditHistoryOptions();
     void setSeratoMetadataEnabled(bool shouldSyncTrackMetadata);
-    void updateDateFormatPreview(const QString& format);
+    void updateDateFormatPreview();
 
     QStandardItemModel m_dirListModel;
     UserSettingsPointer m_pConfig;
@@ -89,5 +89,6 @@ class DlgPrefLibrary : public DlgPreferencePage, public Ui::DlgPrefLibraryDlg {
     // Listen to rate range changes in order to update the fuzzy BPM range
     parented_ptr<ControlProxy> m_pRateRangeDeck1;
 
+    QString m_dateFormat;
     QString m_lastCustomDateFormat;
 };
