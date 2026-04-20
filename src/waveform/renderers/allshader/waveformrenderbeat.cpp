9dd69a48fda1f67b466b33b11e68372cacc1b2ee
ronso0 <ronso0@mixxx.org>
2026-04-20 15:30:21 +0200

    downbeats: use intro_start position, cache position and BeatsPointer

--- src/waveform/renderers/allshader/waveformrenderbeat.cpp
+++ src/waveform/renderers/allshader/waveformrenderbeat.cpp
@@ -23,7 +23,8 @@ namespace allshader {
 WaveformRenderBeat::WaveformRenderBeat(WaveformWidgetRenderer* waveformWidget,
         ::WaveformRendererAbstract::PositionSource type)
         : ::WaveformRendererAbstract(waveformWidget),
-          m_isSlipRenderer(type == ::WaveformRendererAbstract::Slip) {
+          m_isSlipRenderer(type == ::WaveformRendererAbstract::Slip),
+          m_introStartPosCO(ControlFlag::AllowMissingOrInvalid) {
     initForRectangles<RGBAMaterial>(0);
     setUsePreprocess(true);
 }
@@ -34,6 +35,52 @@ void WaveformRenderBeat::setup(const QDomNode& node, const SkinContext& skinCont
     m_downbeatColor = rawDownbeatColor.isEmpty() ? Qt::red : QColor(rawDownbeatColor);
     m_color = WSkinColor::getCorrectColor(m_color).toRgb();
     m_downbeatColor = WSkinColor::getCorrectColor(m_downbeatColor).toRgb();
+    m_introStartPosCO = PollingControlProxy(
+            m_waveformRenderer->getGroup(), QStringLiteral("intro_start_position"));
+}
+
+void WaveformRenderBeat::onSetTrack() {
+    if (m_pLoadedTrack) {
+        disconnect(m_pLoadedTrack.get(),
+                &Track::beatsUpdated,
+                this,
+                &WaveformRenderBeat::slotBeatsUpdated);
+    }
+
+    slotBeatsUpdated();
+
+    const TrackPointer pTrack = m_waveformRenderer->getTrackInfo();
+    if (!pTrack) {
+        return;
+    }
+
+    connect(pTrack.get(),
+            &Track::beatsUpdated,
+            this,
+            &WaveformRenderBeat::slotBeatsUpdated);
+
+    m_pLoadedTrack = pTrack;
+    slotBeatsUpdated();
+}
+
+void WaveformRenderBeat::slotBeatsUpdated() {
+    if (!m_pLoadedTrack) {
+        m_pTrackBeats.reset();
+        m_introCuePos = mixxx::audio::kInvalidFramePos;
+    } else {
+        m_pTrackBeats = m_pLoadedTrack->getBeats();
+        m_introCuePos = mixxx::audio::FramePos::fromEngineSamplePosMaybeInvalid(
+                m_introStartPosCO.get());
+    }
+    setIntroCueBeatPosMaybeInvalid();
+}
+
+void WaveformRenderBeat::setIntroCueBeatPosMaybeInvalid() {
+    if (m_pTrackBeats && m_introCuePos.isValid()) {
+        m_introCueBeatPos = m_pTrackBeats->findClosestBeat(m_introCuePos);
+    } else {
+        m_introCueBeatPos = mixxx::audio::kInvalidFramePos;
+    }
 }
 
 void WaveformRenderBeat::draw(QPainter* painter, QPaintEvent* event) {
@@ -50,18 +97,13 @@ void WaveformRenderBeat::preprocess() {
 }
 
 bool WaveformRenderBeat::preprocessInner() {
-    const TrackPointer trackInfo = m_waveformRenderer->getTrackInfo();
-
-    if (!trackInfo || (m_isSlipRenderer && !m_waveformRenderer->isSlipActive())) {
+    if (!m_pTrackBeats ||
+            (m_isSlipRenderer && !m_waveformRenderer->isSlipActive())) {
         return false;
     }
 
     const double trackSamples = m_waveformRenderer->getTrackSamples();
     if (trackSamples <= 0.0) {
-        return false;
-    }
-    mixxx::BeatsPointer trackBeats = trackInfo->getBeats();
-    if (!trackBeats) {
         return false;
     }
 
@@ -103,11 +145,11 @@ bool WaveformRenderBeat::preprocessInner() {
 
     // Count the number of beats in the range to reserve space in the m_vertices vector.
     // Note that we could also use
-    //   int numBearsInRange = trackBeats->numBeatsInRange(startPosition, endPosition);
+    //   int numBearsInRange = m_pTrackBeats->numBeatsInRange(startPosition, endPosition);
     // for this, but there have been reports of that method failing with a DEBUG_ASSERT.
     int numBeatsInRange = 0;
-    for (auto it = trackBeats->iteratorFrom(startPosition);
-            it != trackBeats->cend() && *it <= endPosition;
+    for (auto it = m_pTrackBeats->iteratorFrom(startPosition);
+            it != m_pTrackBeats->cend() && *it <= endPosition;
             ++it) {
         numBeatsInRange++;
     }
@@ -122,14 +164,26 @@ bool WaveformRenderBeat::preprocessInner() {
     float downbeat_r = m_downbeatColor.redF(),
           downbeat_g = m_downbeatColor.greenF(), downbeat_b = m_color.blueF(),
           downbeat_alpha = m_downbeatColor.alphaF();
-    auto firstBeat = trackBeats->cfirstmarker();
+
+    auto firstDownbeat = m_pTrackBeats->cfirstmarker();
 
     auto* pWaveformWidgetFactory = WaveformWidgetFactory::instance();
     bool downbeatsEnabled = pWaveformWidgetFactory->getDownbeatsEnabled();
     int downbeatLength = pWaveformWidgetFactory->getDownbeatLength();
+    if (downbeatsEnabled) {
+        auto currIntroStartPos = mixxx::audio::FramePos::fromEngineSamplePosMaybeInvalid(
+                m_introStartPosCO.get());
+        if (m_introCuePos != currIntroStartPos) {
+            m_introCuePos = currIntroStartPos;
+            setIntroCueBeatPosMaybeInvalid();
+        }
+        if (m_introCueBeatPos.isValid()) {
+            firstDownbeat = m_pTrackBeats->iteratorFrom(m_introCueBeatPos);
+        }
+    }
 
-    for (auto it = trackBeats->iteratorFrom(startPosition);
-            it != trackBeats->cend() && *it <= endPosition;
+    for (auto it = m_pTrackBeats->iteratorFrom(startPosition);
+            it != m_pTrackBeats->cend() && *it <= endPosition;
             ++it) {
         double beatPosition = it->toEngineSamplePos();
         double xBeatPoint =
@@ -141,7 +195,7 @@ bool WaveformRenderBeat::preprocessInner() {
         const float x1 = static_cast<float>(xBeatPoint);
         const float x2 = x1 + 1.f;
 
-        if (downbeatsEnabled && std::distance(firstBeat, it) % downbeatLength == 0) {
+        if (downbeatsEnabled && std::distance(firstDownbeat, it) % downbeatLength == 0) {
             vertexUpdater.addRectangleHGradient({x1, 0.f},
                     {x2, m_isSlipRenderer ? rendererBreadth / 2 : rendererBreadth},
                     {downbeat_r, downbeat_g, downbeat_b, downbeat_alpha},
