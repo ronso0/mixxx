4e1c80966a98828a414eba7f14a90116b1593ae6
ronso0 <ronso0@mixxx.org>
2026-04-30 17:18:30 +0200

    -+- Browse feature: use symlink map and update it and `watched` flags when library directories changed
    accomplished with QFuture/QFutureWatcher as well as QPromise to avoid trouble with potential multiple calls

--- src/library/treeitem.h
+++ src/library/treeitem.h
@@ -138,6 +138,7 @@ class TreeItem final {
     bool isWatchedLibraryPath() {
         return m_isWatched;
     }
+    void updateIsWatchedLibraryPathRecursively(bool watched);
 
   private:
     explicit TreeItem(
