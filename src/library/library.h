baf3ea8030e32b84a3a969976312d36a1c21cf10
ronso0 <ronso0@mixxx.org>
2026-04-30 17:18:30 +0200

    -+- Browse feature: use symlink map and update it and `watched` flags when library directories changed
    accomplished with QFuture/QFutureWatcher as well as QPromise to avoid trouble with potential multiple calls

--- src/library/library.h
+++ src/library/library.h
@@ -171,6 +171,8 @@ class Library: public QObject {
 
     void onTrackAnalyzerProgress(TrackId trackId, AnalyzerProgress analyzerProgress);
 
+    void trackDirectoriesUpdated();
+
   private slots:
       void onPlayerManagerTrackAnalyzerProgress(TrackId trackId, AnalyzerProgress analyzerProgress);
       void onPlayerManagerTrackAnalyzerIdle();
