e8f20a47f7b74bc3f6849e57ac422a76cea45093
ronso0 <ronso0@mixxx.org>
2026-06-08 20:09:55 +0200

    Library relocate: fix wrong dir

--- src/library/dao/directorydao.cpp
+++ src/library/dao/directorydao.cpp
@@ -255,9 +255,10 @@ std::pair<DirectoryDAO::RelocateResult, QList<RelocatedTrack>> DirectoryDAO::rel
             "UPDATE track_locations SET location=:newloc, directory=:newdir WHERE id=:id";
     query.prepare(replacement);
     for (int i = 0; i < loc_ids.size(); ++i) {
-        query.bindValue(QStringLiteral(":newloc"),
-                relocatedTracks.at(i).updatedTrackRef().getLocation());
-        query.bindValue(QStringLiteral(":newdir"), newDirectory);
+        const QString newLoc = relocatedTracks.at(i).updatedTrackRef().getLocation();
+        query.bindValue(QStringLiteral(":newloc"), newLoc);
+        const QString newDir = newLoc.left(newLoc.lastIndexOf('/'));
+        query.bindValue(QStringLiteral(":newdir"), newDir);
         query.bindValue(QStringLiteral(":id"), loc_ids.at(i).toVariant());
         if (!query.exec()) {
             LOG_FAILED_QUERY(query) << "could not relocate path of track";
