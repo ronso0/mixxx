226a87bac71393376011d0dc860bfceb8db7a92e
ronso0 <ronso0@mixxx.org>
2026-06-16 00:26:13 +0200

    DlgTrackInfoMulti: show progress dialog when writing metadata on close
    This would stall the GUI, because if DlgTrackInfoMulti was the last instance holding the TrackPointers
    it just manipulated, GlobalTrackCache would trigger metadata export to files (if enabled) which might
    take quite some time for many/big files or just slow disk I/O.

--- src/library/dlgtrackinfomulti.h
+++ src/library/dlgtrackinfomulti.h
@@ -39,6 +39,7 @@ class DlgTrackInfoMulti : public QDialog, public Ui::DlgTrackInfoMulti {
     };
 
     void loadTracks(const QList<TrackPointer>& pTracks);
+    QList<TrackPointer> getTracksClearLoadedTracksHash();
     void focusField(const QString& property);
     void prepareFindReplace(const QString& property);
 
