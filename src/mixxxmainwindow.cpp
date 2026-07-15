37340282a503c693edc669583f0fdbd384bf4600
ronso0 <ronso0@mixxx.org>
2026-07-15 16:52:03 +0200

    Scan summary: add missing arg for total missing tracks

--- src/mixxxmainwindow.cpp
+++ src/mixxxmainwindow.cpp
@@ -1315,7 +1315,8 @@ void MixxxMainWindow::slotLibraryScanSummaryDlg(const LibraryScanResultSummary& 
         if (result.numNewMissingTracks != 0) {
             summary += tr("%n track(s) missing (%1 total)",
                     nullptr,
-                    result.numNewMissingTracks);
+                    result.numNewMissingTracks)
+                               .arg(result.numMissingTracks);
         }
         if (result.numRediscoveredTracks != 0) {
             summary += QStringLiteral("<br>") +
